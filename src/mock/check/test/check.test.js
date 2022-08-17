e3267b987f2f64cebad0e27c13d4c313827c5d8d
hansolpark <hansol94@seerslab.com>
2022-08-17 18:04:33 +0900

    add mock function testing

---
+++ src/mock/check/test/check.test.js
@@ -0,0 +1,18 @@
+const check = require('../check');
+
+describe('check', ()=>{
+  let onSuccess;
+  let onFail;
+
+  beforeEach(()=>{
+    onSuccess = jest.fn();
+    onFail = jest.fn();
+  });
+
+  it('should call onSuccess when predicate is true', ()=>{
+    check(()=> true, onSuccess, onFail);
+
+    expect(onSuccess.mock.calls.length).toBe(1);
+  });
+
+});
